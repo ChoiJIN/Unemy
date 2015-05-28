0f1748e8e9f854d755e8902b51d94a3cd519a23d
ChoiJIN <zuharu@gmail.com>
2015-05-29 00:10:00 +0900

    me 그리기까지

--- Unemy/GameState.h
+++ Unemy/GameState.h
@@ -1,14 +1,23 @@
-enum ScreenState {
-	START, MENU, NETWORK, GAME
+enum Screen {
+	START, MENU, GAME, NETWORK
 };
 
-enum GameState {
+enum PlayerState {
+	ALIVE, DEAD
+};
 
+struct Player {
+	int x, y;
+	int size;
+	PlayerState state;
 };
 
 struct Current {
-	ScreenState screen;
-	GameState game;
+	Screen screen;
+	
+	int number;			// 나를 제외한 플레이어 수
+	Player *players;	// 네트워크 플레이어
+	Player me;			// 플레이 유저
 };
 
 Current current;
--- Unemy/Main.cpp
+++ Unemy/Main.cpp
@@ -1,22 +1,36 @@
 #include <windows.h>
+#include <vector>
+#include <string>
 
 #include "CWindow.h"
 
 #include "GameState.h"
 
+using namespace std;
 
 /************************************************************************/
 /* Declaration                                                          */
 /************************************************************************/
-void changeScreen(ScreenState state);
-void changeBackground(ScreenState state);
-char* backgroundFromEnum(ScreenState state);
+void loadImages();						// 모든 이미지를 로딩 함.
+void changeScreen(Screen state);		// Screen 전환 함수
+void changeBackground(Screen state);	// Background 전환 함수
+char* backgroundFromEnum(Screen state);	// Enum으로부터 Background의 파일 경로를 받아옴.
 
 /************************************************************************/
 /* Global Variable                                                      */
 /************************************************************************/
 CWindow window;
 LPDIRECTDRAWSURFACE BackImage;
+struct Color {
+	int r, g, b;
+};
+Color colorKey = { 0, 0, 0 };	// 현재 컬러키가 먹히지 않음. 무조건 검은색으로..
+
+/************************************************************************/
+/* Resources                                                            */
+/************************************************************************/
+vector<LPDIRECTDRAWSURFACE> backgroundImages;
+vector<LPDIRECTDRAWSURFACE> unitImages;
 
 /************************************************************************/
 /* WndProc                                                              */
@@ -29,6 +43,7 @@ LRESULT WndProc(HWND hWnd, LPDIRECTDRAWSURFACE BackScreen, LPDIRECTDRAWSURFACE R
 	case WM_MOUSEMOVE:
 		break;
 	case WM_LBUTTONDOWN:
+		changeScreen(Screen::GAME);
 		break;
 	case WM_DESTROY:
 		window.ReleaseAll();
@@ -47,12 +62,16 @@ LRESULT WndProc(HWND hWnd, LPDIRECTDRAWSURFACE BackScreen, LPDIRECTDRAWSURFACE R
 			PostMessage(hWnd, WM_CLOSE, 0, 0);
 			return 0;
 		case VK_LEFT:
+			current.me.x -= 10;
 			return 0;
 		case VK_RIGHT:
+			current.me.x += 10;
 			return 0;
 		case VK_UP:
+			current.me.y -= 10;
 			return 0;
 		case VK_DOWN:
+			current.me.y += 10;
 			return 0;
 		case VK_SPACE:
 			break;
@@ -70,15 +89,20 @@ LRESULT WndProc(HWND hWnd, LPDIRECTDRAWSURFACE BackScreen, LPDIRECTDRAWSURFACE R
 /************************************************************************/
 /* GameProc                                                             */
 /************************************************************************/
-void GameProc(HWND hWnd, LPDIRECTDRAWSURFACE BackScreen, LPDIRECTDRAWSURFACE RealScreen, int winWidth, int winHeight, bool fullScreen)
+void GameProc(HWND hWnd, LPDIRECTDRAWSURFACE BackScreen, LPDIRECTDRAWSURFACE RealScreen, int winWidth, int winHeight, 
+	bool fullScreen)
 {
 	// Clear Back Ground
 	RECT	BackRect = { 0, 0, winWidth, winHeight };
 	BackScreen->BltFast(0, 0, BackImage, &BackRect, DDBLTFAST_WAIT | DDBLTFAST_SRCCOLORKEY);
 
-	//////////////////////////////
+	//////////////////////////////////////////////////////////////////////////
 
-	//////////////////////////////
+	// Draw me
+	RECT meRect = { 0, 0, 16, 16 };
+	BackScreen->BltFast(current.me.x, current.me.y, unitImages[3], &meRect, DDBLTFAST_WAIT | DDBLTFAST_SRCCOLORKEY);
+
+	//////////////////////////////////////////////////////////////////////////
 
 	if (fullScreen)
 		RealScreen->Flip(NULL, DDFLIP_WAIT);
@@ -101,12 +125,14 @@ int __stdcall WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 	window.SetWndProc(WndProc);
 	window.Init(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
 
+	loadImages();
+
 	// [Game] ////////////////////////////////////////////////////////////////
 
-	current.screen = ScreenState::GAME;
-
-	BackImage = DDLoadBitmap(window.DirectOBJ, backgroundFromEnum(ScreenState::START), 0, 0);
-	DDSetColorKey(BackImage, RGB(0, 0, 0));
+	BackImage = backgroundImages[0];
+	changeScreen(Screen::START);
+	
+	current.me.size = 1;
 
 	//////////////////////////////////////////////////////////////////////////
 
@@ -119,7 +145,7 @@ int __stdcall WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 /************************************************************************/
 /* Implementation                                                       */
 /************************************************************************/
-void changeScreen(ScreenState state)
+void changeScreen(Screen state)
 {
 	if (current.screen == state) {
 		// do nothing
@@ -130,21 +156,20 @@ void changeScreen(ScreenState state)
 	}
 }
 
-void changeBackground(ScreenState state)
+void changeBackground(Screen state)
 {
-	BackImage = DDLoadBitmap(window.DirectOBJ, backgroundFromEnum(state), 0, 0);
-	DDSetColorKey(BackImage, RGB(0, 0, 0));
+// 	BackImage = DDLoadBitmap(window.DirectOBJ, backgroundFromEnum(state), 0, 0);
+// 	DDSetColorKey(BackImage, RGB(0, 0, 0));
+
+	BackImage = backgroundImages[state];
 }
 
-char* backgroundFromEnum(ScreenState state)
+char* backgroundFromEnum(Screen state)
 {
 	switch (state)
 	{
 	case MENU:
 		return "menu.bmp";
-		break;
-	case NETWORK:
-		return "network.bmp";
 		break;
 	case GAME:
 		return "game.bmp";
@@ -152,5 +177,48 @@ char* backgroundFromEnum(ScreenState state)
 	default:
 		return "back.bmp";
 		break;
+	}
+}
+
+void loadImages()
+{
+	LPDIRECTDRAWSURFACE temp;
+
+
+	int background_number = 3;		// number of backgrounds
+	char *background_files[] = {
+		"images/background/back.bmp",
+		"images/background/menu.bmp",
+		"images/background/game.bmp"
+	};
+	for (int i = 0; i < background_number; i++)
+	{
+		temp = DDLoadBitmap(window.DirectOBJ, background_files[i], 0, 0);
+		DDSetColorKey(temp, RGB(colorKey.r, colorKey.g, colorKey.b));
+		backgroundImages.push_back(temp);
+	}
+
+	int unit_number = 1;
+	int size_number = 4;
+	char* unit_files[] = {
+		"images/units/basic/"
+	};
+	char* size_files[] = {
+		"s1.bmp",
+		"s2.bmp",
+		"s3.bmp",
+		"s4.bmp"
+	};
+	for (int i = 0; i < unit_number; i++)
+	{
+		for (int j = 0; j < size_number; j++)
+		{
+			string str(unit_files[i]);
+			str += size_files[j];
+
+			temp = DDLoadBitmap(window.DirectOBJ, str.c_str(), 0, 0);
+			DDSetColorKey(temp, RGB(colorKey.r, colorKey.g, colorKey.b));
+			unitImages.push_back(temp);
+		}
 	}
 }
